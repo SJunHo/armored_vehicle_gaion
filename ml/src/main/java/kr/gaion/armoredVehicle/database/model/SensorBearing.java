c5c2fd98ab5a198b32a4a55d709705ab189e2e62
handaeho <sdf8595@gaion.kr>
2022-11-07 15:20:15 +0900

    예측 위한 unlabeled 데이터의 테이블 Entity 수정 (DB의 ~ DATA 테이블 및 spring의 Sensor ~ .java)

--- ml/src/main/java/kr/gaion/armoredVehicle/database/model/SensorBearing.java
+++ ml/src/main/java/kr/gaion/armoredVehicle/database/model/SensorBearing.java
@@ -1,11 +1,11 @@
 package kr.gaion.armoredVehicle.database.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.*;
-import org.joda.time.DateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.sql.Time;
 import java.util.Date;
 
 @Table(name = "BERDATA")
@@ -16,151 +16,310 @@ import java.util.Date;
 public class SensorBearing {
     @Id
     @GeneratedValue
-    @Column(name="IDX")
+    @Column(name = "IDX")
     private long idx;
 
-    @Column(name="SDAID")
+    @Column(name = "SDAID")
     private String carId;
 
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    @Column(name="OPERDATE")
-    private Date operateDate;
-
-    @JsonFormat(pattern = "HH:mm:ss")
-    @Column(name="OPERTIME")
-    private Time operateTime;
-
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @Column(name="DATE")
+    @Column(name = "DATE")
     private Date operateDateTime;
 
-    @Column(name="TIME")
+    @Column(name = "TIME")
     private long timeIndex;
 
-    @Column(name="AI_Predict")
-    private Double aiPredict;
+    // Ai Predict Values
+    @Column(name = "AI_LBSF")
+    private String aiLbsf;
 
-    @Column(name="AI_Algorithm")
-    private String aiAlgorithm;
+    @Column(name = "AI_LBPFI")
+    private String aiLbpfi;
 
-    @Column(name="AI_Model")
-    private String aiModel;
+    @Column(name = "AI_LBPFO")
+    private String aiLbpfo;
 
-    @Column(name="L_B_V_OverallRMS")
+    @Column(name = "AI_LFTF")
+    private String aiLftf;
+
+    @Column(name = "AI_RBSF")
+    private String aiRbsf;
+
+    @Column(name = "AI_RBPFI")
+    private String aiRbpfi;
+
+    @Column(name = "AI_RBPFO")
+    private String aiRbpfo;
+
+    @Column(name = "AI_RFTF")
+    private String aiRftf;
+
+    // Ai Predict Algorithm
+    @Column(name = "AI_LBSF_ALGO")
+    private String aiLbsfAlgorithm;
+
+    @Column(name = "AI_LBPFI_ALGO")
+    private String aiLbpfiAlgorithm;
+
+    @Column(name = "AI_LBPFO_ALGO")
+    private String aiLbpfoAlgorithm;
+
+    @Column(name = "AI_LFTF_ALGO")
+    private String aiLftfAlgorithm;
+
+    @Column(name = "AI_RBSF_ALGO")
+    private String aiRbsfAlgorithm;
+
+    @Column(name = "AI_RBPFI_ALGO")
+    private String aiRbpfiAlgorithm;
+
+    @Column(name = "AI_RBPFO_ALGO")
+    private String aiRbpfoAlgorithm;
+
+    @Column(name = "AI_RFTF_ALGO")
+    private String aiRftfAlgorithm;
+
+    // Ai Predict Model
+    @Column(name = "AI_LBSF_MODEL")
+    private String aiLbsfModel;
+
+    @Column(name = "AI_LBPFI_MODEL")
+    private String aiLbpfiModel;
+
+    @Column(name = "AI_LBPFO_MODEL")
+    private String aiLbpfoModel;
+
+    @Column(name = "AI_LFTF_MODEL")
+    private String aiLftfModel;
+
+    @Column(name = "AI_RBSF_MODEL")
+    private String aiRbsfModel;
+
+    @Column(name = "AI_RBPFI_MODEL")
+    private String aiRbpfiModel;
+
+    @Column(name = "AI_RBPFO_MODEL")
+    private String aiRbpfoModel;
+
+    @Column(name = "AI_RFTF_MODEL")
+    private String aiRftfModel;
+
+    // Ai Predict Date
+    @Column(name = "AI_LBSF_DATE")
+    private String aiLbsfDate;
+
+    @Column(name = "AI_LBPFI_DATE")
+    private String aiLbpfiDate;
+
+    @Column(name = "AI_LBPFO_DATE")
+    private String aiLbpfoDate;
+
+    @Column(name = "AI_LFTF_DATE")
+    private String aiLftfDate;
+
+    @Column(name = "AI_RBSF_DATE")
+    private String aiRbsfDate;
+
+    @Column(name = "AI_RBPFI_DATE")
+    private String aiRbpfiDate;
+
+    @Column(name = "AI_RBPFO_DATE")
+    private String aiRbpfoDate;
+
+    @Column(name = "AI_RFTF_DATE")
+    private String aiRftfDate;
+
+    // User Judgement Values
+    @Column(name = "USER_LBSF")
+    private String userLbsf;
+
+    @Column(name = "USER_LBPFI")
+    private String userLbpfi;
+
+    @Column(name = "USER_LBPFO")
+    private String userLbpfo;
+
+    @Column(name = "USER_LFTF")
+    private String userLftf;
+
+    @Column(name = "USER_RBSF")
+    private String userRbsf;
+
+    @Column(name = "USER_RBPFI")
+    private String userRbpfi;
+
+    @Column(name = "USER_RBPFO")
+    private String userRbpfo;
+
+    @Column(name = "USER_RFTF")
+    private String userRftf;
+
+    // User Judgement ID
+    @Column(name = "USER_LBSF_ID")
+    private String userLbsfId;
+
+    @Column(name = "USER_LBPFI_ID")
+    private String userLbpfiId;
+
+    @Column(name = "USER_LBPFO_ID")
+    private String userLbpfoId;
+
+    @Column(name = "USER_LFTF_ID")
+    private String userLftfId;
+
+    @Column(name = "USER_RBSF_ID")
+    private String userRbsfId;
+
+    @Column(name = "USER_RBPFI_ID")
+    private String userRbpfiId;
+
+    @Column(name = "USER_RBPFO_ID")
+    private String userRbpfoId;
+
+    @Column(name = "USER_RFTF_ID")
+    private String userRftfId;
+
+    // User Judgement Date
+    @Column(name = "USER_LBSF_DATE")
+    private String userLbsfDate;
+
+    @Column(name = "USER_LBPFI_DATE")
+    private String userLbpfiDate;
+
+    @Column(name = "USER_LBPFO_DATE")
+    private String userLbpfoDate;
+
+    @Column(name = "USER_LFTF_DATE")
+    private String userLftfDate;
+
+    @Column(name = "USER_RBSF_DATE")
+    private String userRbsfDate;
+
+    @Column(name = "USER_RBPFI_DATE")
+    private String userRbpfiDate;
+
+    @Column(name = "USER_RBPFO_DATE")
+    private String userRbpfoDate;
+
+    @Column(name = "USER_RFTF_DATE")
+    private String userRftfDate;
+
+    // Sensor Values
+    @Column(name = "L_B_V_OverallRMS")
     private double lbvOverallRMS;
 
-    @Column(name="L_B_V_1X")
+    @Column(name = "L_B_V_1X")
     private double lbv1x;
 
-    @Column(name="L_B_V_6912BPFO")
+    @Column(name = "L_B_V_6912BPFO")
     private double lbv6912bpfo;
 
-    @Column(name="L_B_V_6912BPFI")
+    @Column(name = "L_B_V_6912BPFI")
     private double lbv6912bpfi;
 
-    @Column(name="L_B_V_6912BSF")
+    @Column(name = "L_B_V_6912BSF")
     private double lbv6912bsf;
 
-    @Column(name="L_B_V_6912FTF")
+    @Column(name = "L_B_V_6912FTF")
     private double lbv6912ftf;
 
-    @Column(name="L_B_V_32924BPFO")
+    @Column(name = "L_B_V_32924BPFO")
     private double lbv32924bpfo;
 
-    @Column(name="L_B_V_32924BPFI")
+    @Column(name = "L_B_V_32924BPFI")
     private double lbv32924bpfi;
 
-    @Column(name="L_B_V_32924BSF")
+    @Column(name = "L_B_V_32924BSF")
     private double lbv32924bsf;
 
-    @Column(name="L_B_V_32924FTF")
+    @Column(name = "L_B_V_32924FTF")
     private double lbv32924ftf;
 
-    @Column(name="L_B_V_32922BPFO")
+    @Column(name = "L_B_V_32922BPFO")
     private double lbv32922bpfo;
 
-    @Column(name="L_B_V_32922BPFI")
+    @Column(name = "L_B_V_32922BPFI")
     private double lbv32922bpfi;
 
-    @Column(name="L_B_V_32922BSF")
+    @Column(name = "L_B_V_32922BSF")
     private double lbv32922bsf;
 
-    @Column(name="L_B_V_32922FTF")
+    @Column(name = "L_B_V_32922FTF")
     private double lbv32922ftf;
 
-    @Column(name="L_B_V_Crestfactor")
+    @Column(name = "L_B_V_Crestfactor")
     private double lbvCrestfactor;
 
-    @Column(name="L_B_V_Demodulation")
+    @Column(name = "L_B_V_Demodulation")
     private double lbvDemodulation;
 
-    @Column(name="L_B_S_Fault1")
+    @Column(name = "L_B_S_Fault1")
     private double lbsFault1;
-    @Column(name="L_B_S_Fault2")
+    @Column(name = "L_B_S_Fault2")
 
     private double lbsFault2;
-    @Column(name="L_B_T_Temperature")
+    @Column(name = "L_B_T_Temperature")
 
     private double lbtTemperature;
-    @Column(name="R_B_V_OverallRMS")
+    @Column(name = "R_B_V_OverallRMS")
 
     private double rbvOverallRMS;
-    @Column(name="R_B_V_1X")
+    @Column(name = "R_B_V_1X")
     private double rbv1x;
 
-    @Column(name="R_B_V_6912BPFO")
+    @Column(name = "R_B_V_6912BPFO")
     private double rbv6912bpfo;
 
-    @Column(name="R_B_V_6912BPFI")
+    @Column(name = "R_B_V_6912BPFI")
     private double rbv6912bpfi;
 
-    @Column(name="R_B_V_6912BSF")
+    @Column(name = "R_B_V_6912BSF")
     private double rbv6912bsf;
-    @Column(name="R_B_V_6912FTF")
+    @Column(name = "R_B_V_6912FTF")
 
     private double rbv6912ftf;
-    @Column(name="R_B_V_32924BPFO")
+    @Column(name = "R_B_V_32924BPFO")
 
     private double rbv32924bpfo;
-    @Column(name="R_B_V_32924BPFI")
+    @Column(name = "R_B_V_32924BPFI")
 
     private double rbv32924bpfi;
-    @Column(name="R_B_V_32924BSF")
+    @Column(name = "R_B_V_32924BSF")
 
     private double rbv32924bsf;
-    @Column(name="R_B_V_32924FTF")
+    @Column(name = "R_B_V_32924FTF")
 
     private double rbv32924ftf;
-    @Column(name="R_B_V_32922BPFO")
+    @Column(name = "R_B_V_32922BPFO")
     private double rbv32922bpfo;
 
-    @Column(name="R_B_V_32922BPFI")
+    @Column(name = "R_B_V_32922BPFI")
     private double rbv32922bpfi;
 
-    @Column(name="R_B_V_32922BSF")
+    @Column(name = "R_B_V_32922BSF")
     private double rbv32922bsf;
 
-    @Column(name="R_B_V_32922FTF")
+    @Column(name = "R_B_V_32922FTF")
     private double rbv32922ftf;
 
-    @Column(name="R_B_V_Crestfactor")
+    @Column(name = "R_B_V_Crestfactor")
     private double rbvCrestfactor;
 
-    @Column(name="R_B_V_Demodulation")
+    @Column(name = "R_B_V_Demodulation")
     private double rbvDemodulation;
 
-    @Column(name="R_B_S_Fault1")
+    @Column(name = "R_B_S_Fault1")
     private double rbsFault1;
 
-    @Column(name="R_B_S_Fault2")
+    @Column(name = "R_B_S_Fault2")
     private double rbsFault2;
 
-    @Column(name="R_B_T_Temperature")
+    @Column(name = "R_B_T_Temperature")
     private double rbtTemperature;
 
-    @Column(name="W_RPM")
+    @Column(name = "W_RPM")
     private double wRpm;
 
-    @Column(name="FILENM")
+    @Column(name = "FILENM")
     private String filenm;
 }
--- ml/src/main/java/kr/gaion/armoredVehicle/database/model/SensorWheel.java
+++ ml/src/main/java/kr/gaion/armoredVehicle/database/model/SensorWheel.java
@@ -4,10 +4,8 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.joda.time.DateTime;
 
 import javax.persistence.*;
-import java.sql.Time;
 import java.util.Date;
 
 @Entity
@@ -18,57 +16,90 @@ import java.util.Date;
 public class SensorWheel {
     @Id
     @GeneratedValue
-    @Column(name="IDX")
+    @Column(name = "IDX")
     private long idx;
 
-    @Column(name="SDAID")
+    @Column(name = "SDAID")
     private String carId;
 
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    @Column(name="OPERDATE")
-    private Date operateDate;
-
-    @JsonFormat(pattern = "HH:mm:ss")
-    @Column(name="OPERTIME")
-    private Time operateTime;
-
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @Column(name="DATE")
+    @Column(name = "DATE")
     private Date operateDateTime;
 
-    @Column(name="TIME")
+    @Column(name = "TIME")
     private long timeIndex;
 
-    @Column(name="AI_Predict")
-    private Double aiPredict;
+    // AI Predict Values
+    @Column(name = "AI_LW")
+    private String aiLw;
 
-    @Column(name="AI_Algorithm")
-    private String aiAlgorithm;
+    @Column(name = "AI_RW")
+    private String aiRw;
 
-    @Column(name="AI_Model")
-    private String aiModel;
+    // Ai Predict Algorithm
+    @Column(name = "AI_LW_ALGO")
+    private String aiLwAlgorithm;
 
-    @Column(name="W_RPM")
+    @Column(name = "AI_RW_ALGO")
+    private String aiRwAlgorithm;
+
+    // Ai Predict Model
+    @Column(name = "AI_LW_MODEL")
+    private String aiLwModel;
+
+    @Column(name = "AI_RW_MODEL")
+    private String aiRwModel;
+
+    // Ai Predict Date
+    @Column(name = "AI_LW_DATE")
+    private String aiLwDate;
+
+    @Column(name = "AI_RW_DATE")
+    private String aiRwDate;
+
+    // User Judgement Values
+    @Column(name = "USER_LW")
+    private String userLw;
+
+    @Column(name = "USER_RW")
+    private String userRw;
+
+    // User Judgement ID
+    @Column(name = "USER_LW_ID")
+    private String userLwId;
+
+    @Column(name = "USER_RW_ID")
+    private String userRwId;
+
+    // User Judgement Date
+    @Column(name = "USER_LW_DATE")
+    private String userLwDate;
+
+    @Column(name = "USER_RW_DATE")
+    private String userRwDate;
+
+    // Sensor Values
+    @Column(name = "W_RPM")
     private double wrpm;
 
-    @Column(name="L_W_V_2X")
+    @Column(name = "L_W_V_2X")
     private double lwv2x;
 
-    @Column(name="L_W_V_3X")
+    @Column(name = "L_W_V_3X")
     private double lwv3x;
 
-    @Column(name="L_W_S_Fault3")
+    @Column(name = "L_W_S_Fault3")
     private double lwsFault3;
 
-    @Column(name="R_W_V_2X")
+    @Column(name = "R_W_V_2X")
     private double rwv2x;
 
-    @Column(name="R_W_V_3X")
+    @Column(name = "R_W_V_3X")
     private double rwv3x;
 
-    @Column(name="R_W_S_Fault3")
+    @Column(name = "R_W_S_Fault3")
     private double rwsFault3;
 
-    @Column(name="FILENM")
+    @Column(name = "FILENM")
     private String filenm;
 }
