35881056a51d78d0f0205694d95dcd03b38fcccc
JunHoSong <41385557+SJunHo@users.noreply.github.com>
2022-12-20 14:17:17 +0900

    쿼리 최적화

--- ml/src/main/java/kr/gaion/armoredVehicle/database/repository/SensorGearboxRepository.java
+++ ml/src/main/java/kr/gaion/armoredVehicle/database/repository/SensorGearboxRepository.java
@@ -16,30 +16,29 @@ public interface SensorGearboxRepository extends JpaRepository<SensorGearbox, Lo
     @Query(value = "Select DISTINCT SDAID from GRBDATA b where ?1 is Not NULL", nativeQuery = true)
     List<String> findDistinctByCarId(String targetColumn);
 
-    @Query(value = "Select `GRBDATA`.IDX, `GRBDATA`.AI_GEAR, `GRBDATA`.AI_GEAR_ALGO, `GRBDATA`.AI_GEAR_MODEL, `GRBDATA`.AI_GEAR_DATE, " +
-            "`GRBDATA`.USER_GEAR, `GRBDATA`.USER_GEAR_ID, `GRBDATA`.USER_GEAR_DATE, " +
-            "`GRBDATA`.W_RPM, `GRBDATA`.G_V_OverallRMS, `GRBDATA`.G_V_Wheel1X, `GRBDATA`.G_V_Wheel2X, " +
-            "`GRBDATA`.G_V_Pinion1X, `GRBDATA`.G_V_Pinion2X, `GRBDATA`.G_V_GMF1X, `GRBDATA`.G_V_GMF2X, " +
-            "`ENGDATA`.AC_h, `ENGDATA`.AC_v, `ENGDATA`.AC_a, `GRBDATA`.`DATE` from `GRBDATA` " +
-            "INNER JOIN `ENGDATA` ON `GRBDATA`.`DATE` = `ENGDATA`.`DATE` AND `GRBDATA`.SDAID = `ENGDATA`.SDAID " +
-            "WHERE `GRBDATA`.AI_GEAR IS NOT NULL AND `GRBDATA`.SDAID = ?1 AND `GRBDATA`.DATE BETWEEN ?2 AND ?3", nativeQuery = true)
+    @Query(value = "Select G.IDX, G.AI_GEAR, G.AI_GEAR_ALGO, G.AI_GEAR_MODEL, G.AI_GEAR_DATE, " +
+            "G.USER_GEAR, G.USER_GEAR_ID, G.USER_GEAR_DATE, " +
+            "G.W_RPM, G.G_V_OverallRMS, G.G_V_Wheel1X, G.G_V_Wheel2X, " +
+            "G.G_V_Pinion1X, G.G_V_Pinion2X, G.G_V_GMF1X, G.G_V_GMF2X, " +
+            "`ENGDATA`.AC_h, `ENGDATA`.AC_v, `ENGDATA`.AC_a, G.`DATE` " +
+            "from (Select * from `GRBDATA`  WHERE `GRBDATA`.AI_GEAR IS NOT NULL AND `GRBDATA`.SDAID = ?1 AND `GRBDATA`.DATE BETWEEN ?2 AND ?3) G " +
+            "INNER JOIN `ENGDATA` ON G.`DATE` = `ENGDATA`.`DATE` AND G.SDAID = `ENGDATA`.SDAID ", nativeQuery = true)
     Page<SensorGearboxInterface> getGearboxAiGearPredictedData(String carId, Date fromDate, Date toDate, Pageable pageable);
 
-    @Query(value = " SELECT GRBDATA.IDX, GRBDATA.AI_GEAR, GRBDATA.AI_GEAR_ALGO, GRBDATA.AI_GEAR_MODEL, GRBDATA.AI_GEAR_DATE, " +
-            " GRBDATA.W_RPM, GRBDATA.G_V_OverallRMS, GRBDATA.G_V_Wheel1X, GRBDATA.G_V_Wheel2X, " +
-            " GRBDATA.G_V_Pinion1X, GRBDATA.G_V_Pinion2X, GRBDATA.G_V_GMF1X, GRBDATA.G_V_GMF2X, " +
-            " ENGDATA.AC_h, ENGDATA.AC_v, ENGDATA.AC_a, GRBDATA.`DATE` " +
-            " FROM `GRBDATA` " +
-            " INNER JOIN `ENGDATA` ON GRBDATA.`DATE` = ENGDATA.`DATE` " +
-            " WHERE GRBDATA.AI_GEAR IS NULL ", nativeQuery = true)
+    @Query(value = " SELECT G.IDX, G.AI_GEAR, G.AI_GEAR_ALGO, G.AI_GEAR_MODEL, G.AI_GEAR_DATE, " +
+            " G.W_RPM, G.G_V_OverallRMS, G.G_V_Wheel1X, G.G_V_Wheel2X, " +
+            " G.G_V_Pinion1X, G.G_V_Pinion2X, G.G_V_GMF1X, G.G_V_GMF2X, " +
+            " ENGDATA.AC_h, ENGDATA.AC_v, ENGDATA.AC_a, G.`DATE` " +
+            " FROM (Select * from `GRBDATA`  WHERE `GRBDATA`.AI_GEAR IS NULL) G " +
+            " INNER JOIN `ENGDATA` ON G.`DATE` = ENGDATA.`DATE` ", nativeQuery = true)
     Page<SensorGearboxInterface> findSensorGearboxAiGEARIsNull(Pageable pageable);
 
     // get G's User judgement values are not Null data
-    @Query(value = "Select `GRBDATA`.IDX, `GRBDATA`.USER_GEAR, `GRBDATA`.USER_GEAR_ID, `GRBDATA`.USER_GEAR_DATE, " +
-            "`GRBDATA`.W_RPM, `GRBDATA`.G_V_OverallRMS, `GRBDATA`.G_V_Wheel1X, `GRBDATA`.G_V_Wheel2X, " +
-            "`GRBDATA`.G_V_Pinion1X, `GRBDATA`.G_V_Pinion2X, `GRBDATA`.G_V_GMF1X, `GRBDATA`.G_V_GMF2X, " +
-            "`ENGDATA`.AC_h, `ENGDATA`.AC_v, `ENGDATA`.AC_a, `GRBDATA`.`DATE` from `GRBDATA` " +
-            "INNER JOIN `ENGDATA` ON `GRBDATA`.`DATE` = `ENGDATA`.`DATE` AND `GRBDATA`.SDAID = `ENGDATA`.SDAID " +
-            "WHERE `GRBDATA`.USER_GEAR IS NOT NULL AND `GRBDATA`.SDAID = ?1 AND `GRBDATA`.DATE BETWEEN ?2 AND ?3", nativeQuery = true)
+    @Query(value = "Select G.IDX, G.USER_GEAR, G.USER_GEAR_ID, G.USER_GEAR_DATE, " +
+            "G.W_RPM, G.G_V_OverallRMS, G.G_V_Wheel1X, G.G_V_Wheel2X, " +
+            "G.G_V_Pinion1X, G.G_V_Pinion2X, G.G_V_GMF1X, G.G_V_GMF2X, " +
+            "`ENGDATA`.AC_h, `ENGDATA`.AC_v, `ENGDATA`.AC_a, G.`DATE` " +
+            "from (Select * from `GRBDATA`  WHERE `GRBDATA`.USER_GEAR IS NOT NULL AND `GRBDATA`.SDAID = ?1 AND `GRBDATA`.DATE BETWEEN ?2 AND ?3) G  " +
+            "INNER JOIN `ENGDATA` ON G.`DATE` = `ENGDATA`.`DATE` AND G.SDAID = `ENGDATA`.SDAID ", nativeQuery = true)
     List<SensorGearboxInterface> getGearboxUserGearData(String carId, Date fromDate, Date toDate);
 }
--- ml/src/main/java/kr/gaion/armoredVehicle/database/repository/SensorWheelRepository.java
+++ ml/src/main/java/kr/gaion/armoredVehicle/database/repository/SensorWheelRepository.java
@@ -17,53 +17,47 @@ public interface SensorWheelRepository extends JpaRepository<SensorWheel, Long> 
     @Query(value = "Select DISTINCT SDAID from WHLDATA b where ?1 is Not NULL", nativeQuery = true)
     List<String> findDistinctByCarId(String targetColumn);
 
-    @Query(value = "Select `WHLDATA`.IDX, `WHLDATA`.AI_LW, `WHLDATA`.AI_LW_ALGO, `WHLDATA`.AI_LW_MODEL, `WHLDATA`.AI_LW_DATE, " +
-            " `WHLDATA`.USER_LW, `WHLDATA`.USER_LW_ID, `WHLDATA`.USER_LW_DATE, " +
-            " `WHLDATA`.W_RPM, `WHLDATA`.L_W_V_2X, `WHLDATA`.L_W_V_3X, `WHLDATA`.L_W_S_Fault3, `WHLDATA`.`DATE`, " +
-            " `ENGDATA`.AC_h, `ENGDATA`.AC_v, `ENGDATA`.AC_a from `WHLDATA` " +
-            " INNER JOIN `ENGDATA` ON `WHLDATA`.`DATE` = `ENGDATA`.`DATE` AND `WHLDATA`.SDAID = `ENGDATA`.SDAID " +
-            " WHERE `WHLDATA`.AI_LW IS NOT NULL AND `WHLDATA`.SDAID = ?1 AND `WHLDATA`.DATE BETWEEN ?2 AND ?3 ", nativeQuery = true)
+    @Query(value = "Select W.IDX, W.AI_LW, W.AI_LW_ALGO, W.AI_LW_MODEL, W.AI_LW_DATE, " +
+            " W.USER_LW, W.USER_LW_ID, W.USER_LW_DATE, " +
+            " W.W_RPM, W.L_W_V_2X, W.L_W_V_3X, W.L_W_S_Fault3, W.`DATE`, " +
+            " `ENGDATA`.AC_h, `ENGDATA`.AC_v, `ENGDATA`.AC_a from (Select * from `WHLDATA`  WHERE `WHLDATA`.AI_LW IS NOT NULL AND `WHLDATA`.SDAID = ?1 AND `WHLDATA`.DATE BETWEEN ?2 AND ?3) W " +
+            " INNER JOIN `ENGDATA` ON W.`DATE` = `ENGDATA`.`DATE` AND W.SDAID = `ENGDATA`.SDAID ", nativeQuery = true)
     Page<SensorWheelLeftInterface> getLeftWheelAiLWPredictedData(String carId, Date fromDate, Date toDate, Pageable pageable);
 
-    @Query(value = "Select `WHLDATA`.IDX, `WHLDATA`.AI_RW, `WHLDATA`.AI_RW_ALGO, `WHLDATA`.AI_RW_MODEL, `WHLDATA`.AI_RW_DATE, " +
-            " `WHLDATA`.USER_RW, `WHLDATA`.USER_RW_ID, `WHLDATA`.USER_RW_DATE, " +
-            " `WHLDATA`.W_RPM, `WHLDATA`.R_W_V_2X, `WHLDATA`.R_W_V_3X, `WHLDATA`.R_W_S_Fault3, " +
-            " `ENGDATA`.AC_h, `ENGDATA`.AC_v, `ENGDATA`.AC_a, `WHLDATA`.`DATE` from `WHLDATA` " +
-            " INNER JOIN `ENGDATA` ON `WHLDATA`.`DATE` = `ENGDATA`.`DATE` AND `WHLDATA`.SDAID = `ENGDATA`.SDAID " +
-            " WHERE `WHLDATA`.AI_RW IS NOT NULL AND `WHLDATA`.SDAID = ?1 AND `WHLDATA`.DATE BETWEEN ?2 AND ?3 ", nativeQuery = true)
+    @Query(value = "Select W.IDX, W.AI_RW, W.AI_RW_ALGO, W.AI_RW_MODEL, W.AI_RW_DATE, " +
+            " W.USER_RW, W.USER_RW_ID, W.USER_RW_DATE, " +
+            " W.W_RPM, W.R_W_V_2X, W.R_W_V_3X, W.R_W_S_Fault3, " +
+            " `ENGDATA`.AC_h, `ENGDATA`.AC_v, `ENGDATA`.AC_a, W.`DATE` from (Select * from `WHLDATA`  WHERE `WHLDATA`.AI_RW IS NOT NULL AND `WHLDATA`.SDAID = ?1 AND `WHLDATA`.DATE BETWEEN ?2 AND ?3) W " +
+            " INNER JOIN `ENGDATA` ON W.`DATE` = `ENGDATA`.`DATE` AND W.SDAID = `ENGDATA`.SDAID ", nativeQuery = true)
     Page<SensorWheelRightInterface> getRightWheelAiRWPredictedData(String carId, Date fromDate, Date toDate, Pageable pageable);
 
     // Wheel Left
-    @Query(value = " SELECT WHLDATA.IDX, WHLDATA.AI_LW, WHLDATA.AI_LW_ALGO, WHLDATA.AI_LW_MODEL, WHLDATA.AI_LW_DATE, " +
-            " WHLDATA.W_RPM, WHLDATA.L_W_V_2X, WHLDATA.L_W_V_3X, WHLDATA.L_W_S_Fault3, " +
-            " ENGDATA.AC_h, ENGDATA.AC_v, ENGDATA.AC_a, WHLDATA.`DATE` " +
-            " FROM `WHLDATA` " +
-            " INNER JOIN `ENGDATA` ON WHLDATA.`DATE` = ENGDATA.`DATE` " +
-            " WHERE WHLDATA.AI_LW IS NULL ", nativeQuery = true)
+    @Query(value = " SELECT W.IDX, W.AI_LW, W.AI_LW_ALGO, W.AI_LW_MODEL, W.AI_LW_DATE, " +
+            " W.W_RPM, W.L_W_V_2X, W.L_W_V_3X, W.L_W_S_Fault3, " +
+            " ENGDATA.AC_h, ENGDATA.AC_v, ENGDATA.AC_a, W.`DATE` " +
+            " FROM (Select * from `WHLDATA`  WHERE `WHLDATA`.AI_LW IS NULL) W " +
+            " INNER JOIN `ENGDATA` ON W.`DATE` = ENGDATA.`DATE` ", nativeQuery = true)
     Page<SensorWheelLeftInterface> findSensorWheelLeftAiLWIsNull(Pageable pageable);
 
     // Wheel Right
-    @Query(value = " SELECT WHLDATA.IDX, WHLDATA.AI_RW, WHLDATA.AI_RW_ALGO, WHLDATA.AI_RW_MODEL, WHLDATA.AI_RW_DATE, " +
-            " WHLDATA.W_RPM, WHLDATA.R_W_V_2X, WHLDATA.R_W_V_3X, WHLDATA.R_W_S_Fault3, " +
-            " ENGDATA.AC_h, ENGDATA.AC_v, ENGDATA.AC_a, WHLDATA.`DATE` " +
-            " FROM `WHLDATA` " +
-            " INNER JOIN `ENGDATA` ON WHLDATA.`DATE` = ENGDATA.`DATE` " +
-            " WHERE WHLDATA.AI_RW IS NULL ", nativeQuery = true)
+    @Query(value = " SELECT W.IDX, W.AI_RW, W.AI_RW_ALGO, W.AI_RW_MODEL, W.AI_RW_DATE, " +
+            " W.W_RPM, W.R_W_V_2X, W.R_W_V_3X, W.R_W_S_Fault3, " +
+            " ENGDATA.AC_h, ENGDATA.AC_v, ENGDATA.AC_a, W.`DATE` " +
+            " FROM (Select * from `WHLDATA`  WHERE `WHLDATA`.AI_RW IS NULL) W " +
+            " INNER JOIN `ENGDATA` ON W.`DATE` = ENGDATA.`DATE` ", nativeQuery = true)
     Page<SensorWheelRightInterface> findSensorWheelRightAiRWIsNull(Pageable pageable);
 
     // get WL's User judgement values are not Null data
-    @Query(value = "Select `WHLDATA`.IDX, `WHLDATA`.USER_LW, `WHLDATA`.USER_LW_ID, `WHLDATA`.USER_LW_DATE, " +
-            " `WHLDATA`.W_RPM, `WHLDATA`.L_W_V_2X, `WHLDATA`.L_W_V_3X, `WHLDATA`.L_W_S_Fault3, `WHLDATA`.`DATE`, " +
-            " `ENGDATA`.AC_h, `ENGDATA`.AC_v, `ENGDATA`.AC_a from `WHLDATA` " +
-            " INNER JOIN `ENGDATA` ON `WHLDATA`.`DATE` = `ENGDATA`.`DATE` AND `WHLDATA`.SDAID = `ENGDATA`.SDAID " +
-            " WHERE `WHLDATA`.USER_LW IS NOT NULL AND `WHLDATA`.SDAID = ?1 AND `WHLDATA`.DATE BETWEEN ?2 AND ?3 ", nativeQuery = true)
+    @Query(value = "Select W.IDX, W.USER_LW, W.USER_LW_ID, W.USER_LW_DATE, " +
+            " W.W_RPM, W.L_W_V_2X, W.L_W_V_3X, W.L_W_S_Fault3, W.`DATE`, " +
+            " `ENGDATA`.AC_h, `ENGDATA`.AC_v, `ENGDATA`.AC_a from (Select * from `WHLDATA`  WHERE `WHLDATA`.USER_LW IS NOT NULL AND `WHLDATA`.SDAID = ?1 AND `WHLDATA`.DATE BETWEEN ?2 AND ?3) W " +
+            " INNER JOIN `ENGDATA` ON W.`DATE` = `ENGDATA`.`DATE` AND W.SDAID = `ENGDATA`.SDAID ", nativeQuery = true)
     List<SensorWheelLeftInterface> getLeftWheelUserLW(String carId, Date fromDate, Date toDate);
 
     // get WR's User judgement values are not Null data
-    @Query(value = "Select `WHLDATA`.IDX, `WHLDATA`.USER_RW, `WHLDATA`.USER_RW_ID, `WHLDATA`.USER_RW_DATE, " +
-            " `WHLDATA`.W_RPM, `WHLDATA`.R_W_V_2X, `WHLDATA`.R_W_V_3X, `WHLDATA`.R_W_S_Fault3, " +
-            " `ENGDATA`.AC_h, `ENGDATA`.AC_v, `ENGDATA`.AC_a, `WHLDATA`.`DATE` from `WHLDATA` " +
-            " INNER JOIN `ENGDATA` ON `WHLDATA`.`DATE` = `ENGDATA`.`DATE` AND `WHLDATA`.SDAID = `ENGDATA`.SDAID " +
-            " WHERE `WHLDATA`.USER_RW IS NOT NULL AND `WHLDATA`.SDAID = ?1 AND `WHLDATA`.DATE BETWEEN ?2 AND ?3 ", nativeQuery = true)
+    @Query(value = "Select W.IDX, W.USER_RW, W.USER_RW_ID, W.USER_RW_DATE, " +
+            " W.W_RPM, W.R_W_V_2X, W.R_W_V_3X, W.R_W_S_Fault3, W.`DATE`, " +
+            " `ENGDATA`.AC_h, `ENGDATA`.AC_v, `ENGDATA`.AC_a from (Select * from `WHLDATA`  WHERE `WHLDATA`.USER_RW IS NOT NULL AND `WHLDATA`.SDAID = ?1 AND `WHLDATA`.DATE BETWEEN ?2 AND ?3) W  " +
+            " INNER JOIN `ENGDATA` ON W.`DATE` = `ENGDATA`.`DATE` AND W.SDAID = `ENGDATA`.SDAID ", nativeQuery = true)
     List<SensorWheelRightInterface> getRightWheelUserRW(String carId, Date fromDate, Date toDate);
 }
